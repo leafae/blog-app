b2a4d9a36248c48398be13f60aabd5c9122b6f53
leafae <hanzi0619@gmail.com>
2025-06-19 12:27:27 +0800

    Changed loading logic using ternary operator

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,18 +24,7 @@ export default function Home() {
   return (
     <div>
       <h1>Blogs List</h1>
-      {isLoading && <h2>Loading blogs...</h2>}
-      <BlogList blogs={blogs} />
-      {/* {blogs.length === 0 ? (
-        <p>No blogs found</p>
-      ) : (
-        blogs.map((blog) => (
-          <div key={blog.id}>
-            <h3>{blog.title}</h3>
-            <p>{blog.content}</p>
-          </div>
-        ))
-      )} */}
+      {isLoading ? <h2>Loading blogs...</h2> : <BlogList blogs={blogs} />}
     </div>
   );
 }
