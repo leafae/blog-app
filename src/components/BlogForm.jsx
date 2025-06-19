56f203aee295b6a6deffe89fe107615d99b41ada
leafae <hanzi0619@gmail.com>
2025-06-19 19:25:12 +0800

    Added CREATE function to add new blog along with form validation

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,24 +1,83 @@
 import React, { useState } from "react";
+import { createBlog } from "../utils/api";
 
 export default function BlogForm() {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({ title: "", content: "" });
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData((currData) => {
+      return {
+        ...currData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleReset = () => {
+    setFormData({ title: "", content: "" });
+    setError("");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    const titleBlank = !formData.title.trim();
+    const contentBlank = !formData.content.trim();
+    if (titleBlank && contentBlank) {
+      setError("⚠︎ Title and Content cannot be blanked");
+      return;
+    } else if (titleBlank) {
+      setError("⚠︎ Title cannot be blanked");
+      return;
+    } else if (contentBlank) {
+      setError("⚠︎ Content cannot be blanked");
+      return;
+    }
+
+    try {
+      const data = await createBlog(formData);
+      console.log("New blog created: ", data);
+      handleReset();
+      alert("New blog addded");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <>
-      <form className="blog-form">
+      <form className="blog-form" onSubmit={handleSubmit}>
         <h1 className="blog-form-title">Add a new Blog!</h1>
         <div className="blog-form-row">
           <label>Title: </label>
-          <input />
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
         </div>
         <div className="blog-form-row">
           <label>Content: </label>
-          <textarea />
+          <textarea
+            name="content"
+            value={formData.content}
+            onChange={handleChange}
+          />
         </div>
         <div className="blog-form-btn-row">
-          <button>Reset</button>
-          <button>Submit</button>
+          <button type="button" onClick={handleReset}>
+            Reset
+          </button>
+          <button type="submit">Submit</button>
         </div>
+        {error && <div className="blog-form-error">{error}</div>}
+        {isSubmitting && <p>Creating new blog...</p>}
       </form>
     </>
   );
