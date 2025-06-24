6464d74c3c50b4623c6116d6a7647106507fdde2
leafae <hanzi0619@gmail.com>
2025-06-24 15:15:46 +0800

    Added sorting button and function to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { getBlogs } from "../utils/api";
 import BlogList from "../components/BlogList";
+import {
+  HiOutlineSortAscending,
+  HiOutlineSortDescending,
+} from "react-icons/hi";
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isDescendingOrder, setIsDescendingOrder] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,10 +26,28 @@ export default function Home() {
     fetchData();
   }, []);
 
+  const toggleSortOrder = () => {
+    setIsDescendingOrder((prev) => !prev);
+  };
+
+  const sortedBlogs = [...blogs].sort((a, b) => {
+    return isDescendingOrder ? b.id - a.id : a.id - b.id;
+  });
+
   return (
     <div>
-      <h1>Blogs List</h1>
-      {isLoading ? <h2>Loading blogs...</h2> : <BlogList blogs={blogs} />}
+      <h1>
+        Blogs List{" "}
+        <span onClick={toggleSortOrder} className="toggle-sort-btn">
+          {isDescendingOrder ? (
+            <HiOutlineSortDescending />
+          ) : (
+            <HiOutlineSortAscending />
+          )}
+        </span>
+      </h1>
+
+      {isLoading ? <h2>Loading blogs...</h2> : <BlogList blogs={sortedBlogs} />}
     </div>
   );
 }
