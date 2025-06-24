cae78385e36067968ad871f0d2d1cb0fcc4d5459
leafae <hanzi0619@gmail.com>
2025-06-24 10:18:37 +0800

    Added update blog feature to edit page

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import BlogForm from "../components/BlogForm";
+import { useParams } from "react-router-dom";
+import { updateBlog } from "../utils/api";
 
 export default function EditBlog() {
+  const { id } = useParams();
+
+  const handleUpdate = async (updatedData) => {
+    return await updateBlog(id, updatedData);
+  };
+
   return (
     <div>
-      <BlogForm formTitle={"Editing blog..."} />
+      <BlogForm formTitle={"Editing blog..."} onSubmit={handleUpdate} />
     </div>
   );
 }
