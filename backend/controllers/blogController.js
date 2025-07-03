434be33a22cf5a5febbe579a7e83e2b189ec4f3a
leafae <hanzi0619@gmail.com>
2025-07-03 14:33:03 +0800

    Refactored and moved blog logic from index.js to routes and controller folders

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,51 @@
+const Blog = require("../models/blog");
+
+const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createBlog = async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateBlog = async (req, res) => {
+  try {
+    const updated = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json(updated);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    const deleted = await Blog.findByIdAndDelete(req.params.id);
+    res.status(200).json(deleted);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getBlogs, getBlog, createBlog, updateBlog, deleteBlog };
--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,6 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-const Blog = require("./models/blog");
-
 const app = express();
 app.use(express.json());
 app.use(
@@ -15,57 +13,8 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/api/blogs", async (req, res) => {
-  try {
-    const blogs = await Blog.find();
-    res.status(200).json(blogs);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/blogs/:id", async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    res.status(200).json(blog);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/api/blogs", async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    res.status(200).json(blog);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.put("/api/blogs/:id", async (req, res) => {
-  try {
-    const updated = await Blog.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json(updated);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    const deleted = await Blog.findByIdAndDelete(req.params.id);
-    res.status(200).json(deleted);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+const blogRoutes = require("./routes/blogRoutes");
+app.use("/api/blogs", blogRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
