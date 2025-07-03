6bacb8be3293f18e44e069bc84464b2bee507aa4
leafae <hanzi0619@gmail.com>
2025-07-03 12:58:51 +0800

    Added GET route to fetch all blogs

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,20 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/api/blogs", async (req, res) => {
   try {
     const blog = await Blog.create(req.body);
     res.status(200).json(blog);
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: error.message });
   }
 });
