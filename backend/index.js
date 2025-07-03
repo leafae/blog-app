0703bae24e260a4838f4d0ee248e8a54b8ceb82a
leafae <hanzi0619@gmail.com>
2025-07-03 14:10:39 +0800

    Enabled CORS for frontend integration

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const Blog = require("./models/blog");
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
