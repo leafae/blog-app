de8fbaa5116339f0f15a45abfa60f2570ec64d84
leafae <hanzi0619@gmail.com>
2025-07-03 12:55:04 +0800

    Added POST route for creating blogs

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(200).json(blog);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
